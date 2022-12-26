c9df906ffd1e42bb883e0f30a78595cce26a7892
YudaKaito <211019@st.yoshida-g.ac.jp>
2022-12-26 16:56:29 +0900

    背景がいい感じに遷移する

--- bg.cpp
+++ bg.cpp
@@ -13,14 +13,41 @@ CBG::~CBG()
 
 HRESULT CBG::Init()
 {
+	alpha = 0.01f;
+	SetTexture(0);
+	for (int i = 0; i < 4; i++)
+	{
+		m_bg[i] = CObject2D::Create(D3DXVECTOR3(CManager::SCREEN_WIDTH * 0.5f, CManager::SCREEN_HEIGHT * 0.5f, 0.0f), D3DXVECTOR3(CManager::SCREEN_WIDTH, CManager::SCREEN_HEIGHT, 0.0f), PRIORITY_BG);
+		m_tree[i][0] = CObject2D::Create(D3DXVECTOR3(CManager::SCREEN_WIDTH * 0.15f, CManager::SCREEN_HEIGHT * 0.5f, 0.0f), D3DXVECTOR3(350.0f, 600.0f, 0.0f), PRIORITY_BG);
+		m_tree[i][1] = CObject2D::Create(D3DXVECTOR3(CManager::SCREEN_WIDTH * 0.85f, CManager::SCREEN_HEIGHT * 0.5f, 0.0f), D3DXVECTOR3(350.0f, 600.0f, 0.0f), PRIORITY_BG);
+		m_tree[i][2] = CObject2D::Create(D3DXVECTOR3(CManager::SCREEN_WIDTH * 0.25f, CManager::SCREEN_HEIGHT * 0.5f, 0.0f), D3DXVECTOR3(325.0f, 550.0f, 0.0f), PRIORITY_BG);
+		m_tree[i][3] = CObject2D::Create(D3DXVECTOR3(CManager::SCREEN_WIDTH * 0.75f, CManager::SCREEN_HEIGHT * 0.5f, 0.0f), D3DXVECTOR3(325.0f, 550.0f, 0.0f), PRIORITY_BG);
+	}
 
-	m_bg = CObject2D::Create(D3DXVECTOR3(CManager::SCREEN_WIDTH * 0.5f, CManager::SCREEN_HEIGHT * 0.5f, 0.0f), D3DXVECTOR3(CManager::SCREEN_WIDTH, CManager::SCREEN_HEIGHT, 0.0f), PRIORITY_BG);
-	m_tree[0] = CObject2D::Create(D3DXVECTOR3(CManager::SCREEN_WIDTH * 0.15f, CManager::SCREEN_HEIGHT * 0.5f, 0.0f), D3DXVECTOR3(350.0f, 600.0f, 0.0f), PRIORITY_BG);
-	m_tree[1] = CObject2D::Create(D3DXVECTOR3(CManager::SCREEN_WIDTH * 0.85f, CManager::SCREEN_HEIGHT * 0.5f, 0.0f), D3DXVECTOR3(350.0f, 600.0f, 0.0f), PRIORITY_BG);
+	m_bg[0]->SetTexture(CTexture::TEXTURE_BG0);
+	m_tree[0][0]->SetTexture(CTexture::TEXTURE_TREE0);
+	m_tree[0][1]->SetTexture(CTexture::TEXTURE_TREE0);
+	m_tree[0][2]->SetTexture(CTexture::TEXTURE_TREE0);
+	m_tree[0][3]->SetTexture(CTexture::TEXTURE_TREE0);
 
-	m_bg->SetTexture(CTexture::TEXTURE_BG0);
-	m_tree[0]->SetTexture(CTexture::TEXTURE_TREE0);
-	m_tree[1]->SetTexture(CTexture::TEXTURE_TREE0);
+	m_bg[1]->SetTexture(CTexture::TEXTURE_BG1);
+	m_tree[1][0]->SetTexture(CTexture::TEXTURE_TREE1);
+	m_tree[1][1]->SetTexture(CTexture::TEXTURE_TREE1);
+	m_tree[1][2]->SetTexture(CTexture::TEXTURE_TREE1);
+	m_tree[1][3]->SetTexture(CTexture::TEXTURE_TREE1);
+
+	m_bg[2]->SetTexture(CTexture::TEXTURE_BG2);
+	m_tree[2][0]->SetTexture(CTexture::TEXTURE_TREE2);
+	m_tree[2][1]->SetTexture(CTexture::TEXTURE_TREE2);
+	m_tree[2][2]->SetTexture(CTexture::TEXTURE_TREE2);
+	m_tree[2][3]->SetTexture(CTexture::TEXTURE_TREE2);
+
+	m_bg[3]->SetTexture(CTexture::TEXTURE_BG3);
+	m_tree[3][0]->SetTexture(CTexture::TEXTURE_TREE3);
+	m_tree[3][1]->SetTexture(CTexture::TEXTURE_TREE3);
+	m_tree[3][2]->SetTexture(CTexture::TEXTURE_TREE3);
+	m_tree[3][3]->SetTexture(CTexture::TEXTURE_TREE3);
+
 	return E_NOTIMPL;
 }
 
@@ -31,6 +58,37 @@ void CBG::Uninit()
 
 void CBG::Update()
 {
+	for (int i = 0; i < 4; i++)
+	{
+		if (select == i)
+		{
+			D3DXCOLOR color = m_bg[i]->GetCol();
+			if (color.a > 1.0f)
+			{
+				continue;
+			}
+			color.a += alpha;
+			m_bg[i]->SetCol(color);
+			m_tree[i][0]->SetCol(color);
+			m_tree[i][1]->SetCol(color);
+			m_tree[i][2]->SetCol(color);
+			m_tree[i][3]->SetCol(color);
+		}
+		else
+		{
+			D3DXCOLOR color = m_bg[i]->GetCol();
+			if (color.a < 0.0f)
+			{
+				continue;
+			}
+			color.a -= alpha;
+			m_bg[i]->SetCol(color);
+			m_tree[i][0]->SetCol(color);
+			m_tree[i][1]->SetCol(color);
+			m_tree[i][2]->SetCol(color);
+			m_tree[i][3]->SetCol(color);
+		}
+	}
 }
 
 void CBG::Draw()
@@ -39,29 +97,5 @@ void CBG::Draw()
 
 void CBG::SetTexture(int i)
 {
-	switch (i)
-	{
-	case 0:
-		m_bg->SetTexture(CTexture::TEXTURE_BG0);
-		m_tree[0]->SetTexture(CTexture::TEXTURE_TREE0);
-		m_tree[1]->SetTexture(CTexture::TEXTURE_TREE0);
-		break;
-	case 1:
-		m_bg->SetTexture(CTexture::TEXTURE_BG1);
-		m_tree[0]->SetTexture(CTexture::TEXTURE_TREE1);
-		m_tree[1]->SetTexture(CTexture::TEXTURE_TREE1);
-		break;
-	case 2:
-		m_bg->SetTexture(CTexture::TEXTURE_BG2);
-		m_tree[0]->SetTexture(CTexture::TEXTURE_TREE2);
-		m_tree[1]->SetTexture(CTexture::TEXTURE_TREE2);
-		break;
-	case 3:
-		m_bg->SetTexture(CTexture::TEXTURE_BG3);
-		m_tree[0]->SetTexture(CTexture::TEXTURE_TREE3);
-		m_tree[1]->SetTexture(CTexture::TEXTURE_TREE3);
-		break;
-	default:
-		break;
-	}
+	select = i;
 }
